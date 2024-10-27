1a4b05e502d9ec3e8cb1e1940f305ad642829b65
saboritani <blackey2309@gmail.com>
2024-10-27 17:27:19 +0900

    fix tab and shift_width, and etc.

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.o.startofline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4
+vim.o.shiftwidth = 0
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.autoindent = false
