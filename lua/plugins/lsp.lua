f048e7fb48098d0b43d96ddee5afcbfc3c3b24a1
saboritani <blackey2309@gmail.com>
2024-10-28 23:06:52 +0900

    setup mason and mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 if not vim.g.vscode then
-    return {           
+    return {
         {
             "williamboman/mason.nvim",
             build = ":MasonUpdate",
@@ -12,35 +12,44 @@ if not vim.g.vscode then
                 { "neovim/nvim-lspconfig" },
             },
             config = function()
-            local lspconfig = require("lspconfig")
-            require('mini.completion').setup({})
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup(opts)
-                end,
-                ["vtsls"] = function()
-                    lspconfig["vtsls"].setup({})
-                end,
-            })
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(_)
-                vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-                vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-                vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-                vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-                vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-                vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-                vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-                vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-                end
-                })
-            vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-                vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-            )
+                local mason = require("mason")
+                local mason_lspconfig = require("mason-lspconfig")
+                local lspconfig = require("lspconfig")
+
+                mason.setup()
+                mason_lspconfig.setup()
+
+                mason_lspconfig.setup_handlers {
+                    -- The first entry (without a key) will be the default handler
+                    -- and will be called for each installed server that doesn't have
+                    -- a dedicated handler.
+                    function (server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {}
+                    end,
+                    -- Next, you can provide a dedicated handler for specific servers.
+                    -- For example, a handler override for the `rust_analyzer`:
+                }
+
+
+                vim.api.nvim_create_autocmd("LspAttach", {
+                    callback = function(_)
+                    vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+                    vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+                    vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+                    vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+                    vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+                    vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+                    vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+                    vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+                    end
+                    })
+                vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+                    vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+                )
             end
         }
     }
